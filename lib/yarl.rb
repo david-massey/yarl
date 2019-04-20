dc282869c8a24f7c1185f7b7a27a361f449d78bb
David Massey <david@controlenvy.com>
2019-04-20 17:23:52 -0400

    Added NOTICE level. Added regex to convert non-printable characters to hex. Added support to convert extended ascii to hex. Added body color support.

--- lib/yarl.rb
+++ lib/yarl.rb
@@ -4,6 +4,10 @@ class YARL < Logger # Yet Another Ruby Logger
   module Severity
     include Logger::Severity
 
+    # Normal but significant conditions
+    # Conditions that are not error/warning conditions, but that may require special handling.
+    NOTICE = 1.5
+
     # For that time you need more messages that annoy the hell out of everyone else.
     # These should rarely -- if ever -- be displayed.
     SPAM = -1
@@ -11,37 +15,47 @@ class YARL < Logger # Yet Another Ruby Logger
 
   include Severity
 
-  def initialize(progname = nil, **kwargs)
+  def initialize progname = nil, **kwargs
     destination = kwargs[:destination] || STDOUT
     super destination
 
-    @progname  = progname.nil? ? self.class : progname
-    @level = kwargs[:level] || INFO
+    @progname = progname.nil? ? self.class : progname
+    @level    = kwargs[:level] || INFO
 
     # Header colors
-    @color      =       text_color(kwargs[:color] || :light_white)
-    @background = background_color(kwargs[:background] || :black)
+    color       =       text_color(kwargs[:color] || kwargs[:header])
+    background  = background_color(kwargs[:background])
+    @header     = format_code color, background
 
-    # Body colors
-    @fatal_color = background_color(kwargs[:fatal_color] || :red)
-    @error_color =       text_color(kwargs[:error_color] || :red)
-    @spam_color  =       text_color(kwargs[:spam_color]  || :bright_black)
+    # Default body colors
+    @body       =       text_color(kwargs[:body])
+
+    # Automatic body colors
+    @fatal_body = background_color(kwargs[:fatal] || kwargs[:fatal_color] || :red)
+    @error_body =       text_color(kwargs[:error] || kwargs[:error_color] || :red)
+    @warn_body  =       text_face (kwargs[:warn]  || kwargs[:warn_face]   || :bold)
+    @spam_body  =       text_color(kwargs[:spam]  || kwargs[:spam_color]  || :bright_black)
 
     # Log formatting
     @datetime_format = kwargs[:datetime_format] || '%Y-%m-%dT%H:%M:%S.%3NZ'
+    if kwargs[:ascii8bit]
+      @to_hex_regex = /([^ -~])/no
+    elsif !(kwargs[:printable] === false)
+      @to_hex_regex = /([\x00-\x19])/o
+    end
 
     if kwargs[:formatter].is_a?(Proc)
       @formatter = kwargs[:formatter]
     else
       @formatter ||= proc { |severity, datetime, progname, message|
         datetime = datetime.strftime(@datetime_format)
-        "\e[#{@color};#{@background}m#{datetime} #{severity[0]} #{progname}\e[0m #{message}\n"
+        "\e[#{@header}m#{datetime} #{severity[0]} #{progname}\e[#{@body}m #{message}\e[0m\n"
       }
     end
   end
 
   # A basic rewrite of Logger.add to support message body colors.
-  def add(severity, message = nil, progname = nil)
+  def add severity, message = nil, progname = nil
     severity ||= UNKNOWN
     return true if @logdev.nil? or severity < @level
 
@@ -50,13 +64,17 @@ class YARL < Logger # Yet Another Ruby Logger
 
     progname = @progname if message == progname
 
+    message.gsub!(@to_hex_regex) {|c| "[%02X]" % c.ord} if @to_hex_regex
+
     case severity
     when FATAL
-      message = "\e[#{@fatal_color}m#{message}\e[0m"
+      message = "\e[#{@fatal_body}m#{message}"
     when ERROR
-      message = "\e[#{@error_color}m#{message}\e[0m"
+      message = "\e[#{@error_body}m#{message}"
+    when WARN
+      message = "\e[#{@warn_body}m#{message}"
     when SPAM
-      message = "\e[#{@spam_color}m#{message}\e[0m"
+      message = "\e[#{@spam_body}m#{message}"
     end
 
     @logdev.write(
@@ -71,12 +89,14 @@ class YARL < Logger # Yet Another Ruby Logger
     true
   end
 
-  # SPAM support
-  # Add SPAM to level setter
-  def level=(severity)
+  # SPAM and NOTICE support
+  # Add to level setter
+  def level= severity
     severity = severity.to_s if severity.is_a?(Symbol)
 
     case severity
+    when 'notice'
+      @level = NOTICE
     when 'spam'
       @level = SPAM
     else
@@ -84,14 +104,20 @@ class YARL < Logger # Yet Another Ruby Logger
     end
   end
 
-  # Returns +true+ iff the current severity level allows for the printing of SPAM
+  # Returns +true+ iff the current severity level allows
+  def notice?; @level <= NOTICE; end
   def spam?; @level <= SPAM; end
 
   # Sets the severity to SPAM.
+  def notice!; self.level = NOTICE; end
   def spam!; self.level = SPAM; end
 
-  # Log a +DEBUG+ message.
-  def spam(progname = nil, &block)
+  # Log a +NOTICE+ message.
+  def notice progname = nil, &block
+    add(NOTICE, nil, progname, &block)
+  end
+  # Log a +SPAM+ message.
+  def spam progname = nil, &block
     add(SPAM, nil, progname, &block)
   end
 
@@ -101,23 +127,50 @@ class YARL < Logger # Yet Another Ruby Logger
     SPAM    => 'SPAM',
     DEBUG   => 'DEBUG',
     INFO    => 'INFO',
+    NOTICE  => 'NOTICE',
     WARN    => 'WARN',
     ERROR   => 'ERROR',
     FATAL   => 'FATAL',
     UNKNOWN => 'UNKWN'
   }
 
-  def format_severity(severity)
+  def format_severity severity
     SEV_LABEL[severity] || 'ANY'
   end
 
-  # Color Methods
-  def text_color(color)
-    (COLOR_CODES[color] || 67) + 30
+  def format_code color, background = 0
+    if color > 0 && background > 0
+      "#{color};#{background}"
+    elsif color > 0
+      "#{color}"
+    elsif background > 0
+      "#{background}"
+    else
+      '0'
+    end
   end
 
-  def background_color(color)
-    (COLOR_CODES[color] ||  0) + 40
+  # Font Face Methods
+  def text_face mode
+    return FACE_MODES[mode]
+  end
+
+  FACE_MODES = {
+    :reset      => 0,
+    :bold       => 1,
+    :underscore => 4,
+    :blink      => 5 # Please don't...
+  }
+
+  # Color Methods
+  def text_color color
+    return (COLOR_CODES[color] || 67) + 30 if color
+    return 0
+  end
+
+  def background_color color
+    return (COLOR_CODES[color] ||  0) + 40 if color
+    return 0
   end
 
   COLOR_CODES = {
