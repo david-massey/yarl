1ee139e6f4ea99ac316a4675fa3128d7737fbe6b
David Massey <david@controlenvy.com>
2019-08-19 16:27:32 -0400

    Hard-coded logger to 1.3 due to error in logger 1.4.0.

--- lib/yarl.rb
+++ lib/yarl.rb
@@ -64,7 +64,7 @@ class YARL < Logger # Yet Another Ruby Logger
 
     progname = @progname if message == progname
 
-    message = message.gsub(@to_hex_regex) {|c| "[%02X]" % c.ord} if @to_hex_regex
+    message = message.to_s.gsub(@to_hex_regex) {|c| "[%02X]" % c.ord} if @to_hex_regex
 
     case severity
     when FATAL
